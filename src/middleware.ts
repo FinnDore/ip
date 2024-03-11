7eb61869c03eed29420ebb08aa2a6251336f3013
Finn <34718806+FinnDore@users.noreply.github.com>
2024-03-11 18:12:24 +0000

    :mag:

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,11 @@ import { NextResponse } from 'next/server';
 
 export default function middleware(req: Request) {
     const params = new URLSearchParams({
-        text: `My IP is ${req.headers.get('x-forwarded-for') ?? 'unknown'}!`,
+        text: `My IP is ${
+            req.headers.get('x-vercel-forwarded-for') ??
+            req.headers.get('x-forwarded-for') ??
+            'unknown'
+        }!`,
     } as Record<string, string>);
     return NextResponse.redirect(
         `https://x.com/intent/tweet?${params.toString()}`
