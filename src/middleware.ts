077f280494ea445a7b049edac8622b07d38114d2
Finn <34718806+FinnDore@users.noreply.github.com>
2024-03-11 18:17:55 +0000

    :mag:

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { NextResponse } from 'next/server';
 export default function middleware(req: Request) {
     const params = new URLSearchParams({
         text: `My IP is ${
+            req.headers.get('CF-Connecting-IP') ??
             req.headers.get('x-vercel-forwarded-for') ??
             req.headers.get('x-forwarded-for') ??
             'unknown'
